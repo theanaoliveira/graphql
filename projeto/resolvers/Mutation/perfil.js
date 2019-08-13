770a05b9e6da2d279ace6e90c01b70f6e1ac98ad
Ana Caroline de Oliveira <ana.oliveira2_vivo@tiglobalweb.onmicrosoft.com>
2019-08-13 09:54:48 -0300

    fixed file

---
+++ projeto/resolvers/Mutation/perfil.js
@@ -0,0 +1,54 @@
+const { perfis, proximoId } = require("../../data/db")
+
+function findProfile(filtro) {
+    if (!filtro) return -1
+
+    const {id, nome} = filtro
+
+    return perfis.findIndex(u=> u.id === id) == -1 ? 
+        perfis.findIndex(u=> u.nome == nome) : 
+        perfis.findIndex(u=> u.id === id);
+}
+
+module.exports = {
+    novoPerfil(_, { dados }) {
+        const perfilExistente = perfis.some(u=> u.nome.toLowerCase() === dados.nome.toLowerCase());
+
+        console.log(dados)
+
+        if (perfilExistente)
+            throw new Error('Perfil já cadastrado')
+
+        const perfil = {
+            id: proximoId,
+            ...dados
+        }
+
+        perfis.push(perfil);
+        return perfil;
+    },
+
+    alterarPerfil(_, { filtro, dados }) {
+        const i = findProfile(filtro)
+
+        if (i < 0) return null
+
+        const perfil = {
+            ...perfis[i],
+            id: perfis[i].id,
+            ...dados
+        }
+
+        perfis.splice(i, 1, perfil)
+        return perfil
+    },
+
+    excluirPerfil(_, { filtro }) {
+        const i = findProfile(filtro)
+
+        if (i < 0) return null
+
+        const perfil = perfis.splice(i, 1)
+        return perfil ? perfil[0] : null
+    }
+}
