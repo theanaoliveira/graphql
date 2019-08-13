60ec63a609bd9dca1a2a5453ad1447151120bfda
Ana Caroline de Oliveira <ana.oliveira2_vivo@tiglobalweb.onmicrosoft.com>
2019-08-13 11:09:28 -0300

    included solution

--- graphql-netcore/GraphQL/GraphQL.Webapi/Program.cs
+++ graphql-netcore/GraphQL/GraphQL.Webapi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace GraphQL_NetCore
+namespace GraphQL.Webapi
 {
     public class Program
     {
--- graphql-netcore/GraphQL/GraphQL.Webapi/Startup.cs
+++ graphql-netcore/GraphQL/GraphQL.Webapi/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace GraphQL_NetCore
+namespace GraphQL.Webapi
 {
     public class Startup
     {
